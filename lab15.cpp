d645fef73093859a0d0b025dc9886b53e53a2c58
kkawayuki <kyukiokawa@gmail.com>
2024-09-29 17:02:07 -0700

    finished commenting convention

--- lab15.cpp
+++ lab15.cpp
@@ -1,3 +1,4 @@
+// COMSC-210 | lab 15 | Kent Kawashima
 #include <iostream>
 #include <array>
 #include <fstream>
@@ -13,14 +14,17 @@ private:
     string title;
 
 public:
+    // inline functions to get each private value, const to avoid loophole changes
     string getWriter() const { return writer; }
     int getYear() const { return year; }
     string getTitle() const { return title; }
 
+    // setter inline functions for each seperate variable
     void setWriter(string name) { writer = name; }
     void setYear(int num) { year = num; }
     void setTitle(string name) { title = name; }
 
+    // method to print out all values of specified class, formatted neatly.
     void printAll() const
     {
         cout << "Movie: " << writer << '\n';
@@ -36,12 +40,13 @@ const int SIZE = 4; // max capacity, edit based on input.txt
 void readInMovies(array<Movie, SIZE> &movies);
 
 /**************************************
- * Fucntion: Main
+ * Function: Main
  **************************************/
 int main()
 {
     array<Movie, SIZE> movies;
 
+    // read in data from txt file, fill array
     readInMovies(movies);
 
     // output array elements
@@ -51,6 +56,17 @@ int main()
     }
 }
 
+/************************************************
+ * Function: Takes in the original array holding
+ * Movie information from origin of call, fills
+ * each of SIZE elements with data from a formatted
+ * txt file input, ensuring that the file exists
+ * and is open. Afterwards, closes the txt file.
+ *
+ * Parameters: &movies, the array of movies, passed
+ * by reference so its elements can be modified
+ * Return: NONE
+ ************************************************/
 void readInMovies(array<Movie, SIZE> &movies)
 {
     Movie temp;
@@ -60,14 +76,15 @@ void readInMovies(array<Movie, SIZE> &movies)
     ifstream in;
     in.open("input.txt");
 
-    if (!in.good()) // validation
+    if (!in.good()) // file validation
     {
         cout << "Error opening text file\n";
     }
     else
     {
-        for (int i = 0; i < SIZE; i++)
+        for (int i = 0; i < SIZE; i++) // for i=0 -> i=4
         {
+            // read in/set title, year, and writer respectively using public setter functions
             getline(in, buf);
             temp.setTitle(buf);
 
@@ -78,7 +95,7 @@ void readInMovies(array<Movie, SIZE> &movies)
             getline(in, buf);
             temp.setWriter(buf);
 
-            movies[i] = temp;
+            movies[i] = temp; // set specific element to temporary Movie
         }
     }
     in.close();
